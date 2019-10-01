bb545e06f25e92f235d0ed2450eb3cd47654814d
Fabian Zimmermann <F_Zimmermann@gmx.net>
2019-10-01 20:53:31 +0200

    push add to repository

---
+++ Assets/Scripts/CustomPhotonLauncher.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class CustomPhotonLauncher : MonoBehaviourPunCallbacks
+{
+
+    string gameVersion = "1";
+    PhotonView myPhotonView;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Connect();
+        myPhotonView = GetComponent<PhotonView>();
+    }
+
+    public void Connect()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.GameVersion = gameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        gameObject.GetComponent<SpatialMeshHandler>().meshObserver.Suspend();
+        Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
+    }
+
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+
+        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        PhotonNetwork.CreateRoom(null, new RoomOptions());
+    }
+
+    public override void OnJoinedRoom()
+    {
+        gameObject.GetComponent<SpatialMeshHandler>().meshObserver.Resume();
+
+
+        Debug.Log(" OnJoinedRoom() called by PUN. Now this client is in a room.");
+
+    }
+
+}
---
+++ Assets/Scripts/SpatialMeshHandler.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using Microsoft.MixedReality.Toolkit.Input;
+using Microsoft.MixedReality.Toolkit.SpatialAwareness;
+using Microsoft.MixedReality.Toolkit.Utilities;
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpatialMeshHandler : MonoBehaviourPun, IMixedRealitySpatialAwarenessObservationHandler<SpatialAwarenessMeshObject>
+{
+    private PhotonView RPCPhotonView;
+    private Dictionary<int, int> meshDataUpdates;
+
+    void Start()
+    {
+        RPCPhotonView = GetComponent<PhotonView>();
+        meshDataUpdates = new Dictionary<int, int>();
+
+    }
+
+    //for accessing SpatialAwarenessSystem
+    private IMixedRealitySpatialAwarenessSystem spatialAwarenessSystem = null;
+
+    //for accessing DataProvider
+    private IMixedRealityDataProviderAccess dataProviderAccess = null;
+
+    //for accessing MeshObserver
+    public IMixedRealitySpatialAwarenessMeshObserver meshObserver = null;
+
+    public IMixedRealitySpatialAwarenessSystem SpatialAwarenessSystem
+    {
+        get
+        {
+            if (spatialAwarenessSystem == null)
+            {
+                MixedRealityServiceRegistry.TryGetService<IMixedRealitySpatialAwarenessSystem>(out spatialAwarenessSystem);
+            }
+            return spatialAwarenessSystem;
+        }
+    }
+
+    public IMixedRealityDataProviderAccess DataProviderAccess
+    {
+        get
+        {
+            if (dataProviderAccess == null)
+            {
+                dataProviderAccess = spatialAwarenessSystem as IMixedRealityDataProviderAccess;
+            }
+            return dataProviderAccess;
+        }
+    }
+
+    private IMixedRealitySpatialAwarenessMeshObserver MeshObserver
+    {
+        get
+        {
+            if (dataProviderAccess != null)
+                meshObserver = dataProviderAccess.GetDataProvider<IMixedRealitySpatialAwarenessMeshObserver>();
+            else
+                Debug.Log("DataAccessProvider is null");
+            return meshObserver;
+        }
+       
+    }
+
+   
+
+    private async void OnEnable()
+    {
+        await new WaitUntil(() => SpatialAwarenessSystem != null);
+        SpatialAwarenessSystem.Register(gameObject);
+        await new WaitUntil(() => DataProviderAccess != null);
+        await new WaitUntil(() => MeshObserver != null);     
+    }
+
+    private void OnDisable()
+    {
+        SpatialAwarenessSystem?.Unregister(gameObject);
+    }
+
+  
+    public void OnObservationAdded(MixedRealitySpatialAwarenessEventData<SpatialAwarenessMeshObject> eventData)
+    {
+        if (!meshDataUpdates.ContainsKey(eventData.Id))
+        {
+            //add mesh id to Dictionary, so we can count updates
+            meshDataUpdates.Add(eventData.Id, 0);
+        }
+    }
+
+    public void OnObservationRemoved(MixedRealitySpatialAwarenessEventData<SpatialAwarenessMeshObject> eventData)
+    {
+        if(meshDataUpdates.ContainsKey(eventData.Id))
+            {
+            RPCPhotonView.RPC("RemoveMesh", RpcTarget.Others, eventData.Id);          
+            meshDataUpdates.Remove(eventData.Id);
+            }
+    }
+
+    public void OnObservationUpdated(MixedRealitySpatialAwarenessEventData<SpatialAwarenessMeshObject> eventData)
+    {
+        int updateCount = 0;
+
+        //if mesh was added to observation...
+        if(meshDataUpdates.TryGetValue(eventData.Id, out updateCount))
+        {
+            //...update the count
+            meshDataUpdates[eventData.Id] = ++updateCount;
+
+            //for every third update...
+            if ((updateCount % 3) == 0)
+            {
+                //...send vertices, triangles and uvs to reciever app via RPC
+                RPCPhotonView.RPC("RecieveMeshData", RpcTarget.Others, eventData.SpatialObject.Filter.mesh.vertices, eventData.SpatialObject.Filter.mesh.triangles,
+                    eventData.SpatialObject.Filter.mesh.uv, eventData.SpatialObject.Id);
+            }
+        }
+
+
+    }
+
+    [PunRPC]
+    public void SendMeshData()
+    {
+
+    }
+
+}
